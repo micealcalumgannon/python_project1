9e5c6b36a8821900ed82a858a12e8d618500f353
micealcalumgannon <nitroexfusion@gmail.com>
2023-08-01 22:54:19 +0100

    second commit

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
 print('hola world')
+
+def squaring_number(number):
+    squared_number = number * number
+    return squared_number
+
+input_value = int(input('Input the number you wish to be squared'))
+
+my_squared_number = squaring_number(input_value)
+if my_squared_number < 10:
+    print('this number is below 10')
+else:
+    print('This number is different')
